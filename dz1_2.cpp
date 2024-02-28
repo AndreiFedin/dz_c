b0bfd23e3b4c8238a1f5bb08e3394137cc5145b6
AndreiFedin <fedin.87@mail.ru>
2024-02-28 14:10:50 +0300

    homelab1

---
+++ dz1_2.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main( int argc, char *argv[] ) {
+    printf( "Let's\n   go\n     to walk" ) ;
+    printf( "   go\n" ) ;
+    printf( "     to walk" ) ;
+    system("pause>nul") ;
+    return 0 ;
+}
---
+++ dz1_3.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main( int argc, char *argv[] ) {
+    printf( "   *   \n" ) ;
+    printf( "  ***  \n" ) ;
+    printf( " ***** \n" ) ;
+    printf( "*******\n" ) ;
+    printf( " HH HH \n" ) ;
+    printf( " ZZZZZ \n" ) ;
+    
+    system("pause>nul") ;
+    return 0 ;
+}
