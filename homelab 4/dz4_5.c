bfda00d260598187dda1013d876b7af25e3bae04
AndreiFedin <fedin.87@mail.ru>
2024-03-05 18:53:07 +0300

    Update dz4_5.c

--- homelab 4/dz4_5.c
+++ homelab 4/dz4_5.c
@@ -9,16 +9,14 @@ int main(int argc, char *argv[]) {
     
     scanf("%f  %f  %f  %f", &x1, &y1, &x2, &y2) ;
     
-	if ( x1!=x2 ) {
-	
+    if ( x1!=x2 ) {
         k = (y2-y1)/(x2-x1) ;
         b = x2*k-y2 ;
-    
         printf("%.2f  %.2f", k, -b) ;
     }
-	else
-	    printf("incorrect coordinates") ;
-		
+    else
+        printf("incorrect coordinates") ;
+        
     system("pause>nul") ;
-	return 0;
+    return 0;
 }
