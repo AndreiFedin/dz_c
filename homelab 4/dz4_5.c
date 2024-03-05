259e21ab80cd5ae867c4e8dba3e3fe3a8d223d5a
AndreiFedin <fedin.87@mail.ru>
2024-03-05 18:47:54 +0300

    homelab

--- homelab 4/dz4_5.c
+++ homelab 4/dz4_5.c
@@ -1,16 +1,24 @@
-#int main(int argc, char *argv[]) {
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
     
     float x1,y1,x2,y2 ;
     float k,b ;
     
     scanf("%f  %f  %f  %f", &x1, &y1, &x2, &y2) ;
     
-    k = (y2-y1)/(x2-x1) ;
+	if ( x1!=x2 ) {
+	
+        k = (y2-y1)/(x2-x1) ;
+        b = x2*k-y2 ;
     
-    b = y2/k-x2 ;
-    
-    printf("%.2f  %.2f", k, b) ;
-    
+        printf("%.2f  %.2f", k, -b) ;
+    }
+	else
+	    printf("incorrect coordinates") ;
+		
     system("pause>nul") ;
 	return 0;
 }
